0b8f240b17e20ce7ad5d36b8ba16aac3cdc48263
suisrc <z@suisrc.cn>
2020-04-22 19:19:11 +0800

    项目初始化狗i按

--- kratos-ui/.umirc.ts
+++ kratos-ui/.umirc.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'umi';
+
 export default defineConfig({
-  antd: {
-    compact: true,
-  },
-  dva: {
-    immer: true,
-    hmr: false,
-  },
-  title: "Kratos",
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [
-    {
-      name: '登录页',
-      path: '/ks/user/signin',
-      component: './UserLogin',
-    },
-    {
-      path: '/',
-      component: '@/pages/index',
-    },
+    { path: '/', component: '@/pages/index' },
   ],
 });
